156034a5a68061c1a4c63c12399e4ac47ed900c2
Raul Gonzalez <ragonlan@gmail.com>
2020-06-08 13:41:45 +0200

    Get security updates in system

--- lib/facter/yum_updates.rb
+++ lib/facter/yum_updates.rb
@@ -22,6 +22,30 @@ Facter.add('yum_package_updates') do
   end
 end
 
+Facter.add('yum_security_updates') do
+  confine osfamily: 'RedHat'
+  setcode do
+    yum_security_updates = Hash.new(Array.new())
+    if File.executable?('/usr/bin/yum')
+      yum_get_security_result = Facter::Util::Resolution.exec('/usr/bin/yum --quiet updateinfo list security installed') #TODO: --secseverity=Moderate
+      unless yum_get_security_result.nil?
+        yum_get_security_result.each_line do |line|
+          _sec_code, _sec_level, package, trash = line.split(%r{\s+})
+          if trash  #some repositories make yum fill with garbage
+            next
+          end
+          _sec_level.chomp!("/Sec.")
+          if not yum_security_updates.key?(_sec_level)
+            yum_security_updates[_sec_level] = Array.new()
+          end
+          yum_security_updates[_sec_level].push(package)
+        end
+      end
+    end
+    yum_security_updates
+  end
+end
+
 Facter.add('yum_has_updates') do
   confine osfamily: 'RedHat'
   setcode do
