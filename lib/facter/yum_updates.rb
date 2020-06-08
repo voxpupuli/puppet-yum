bff4578c4eea09e619002d8d8708b1ceb0335977
Raul Gonzalez <ragonlan@gmail.com>
2020-06-08 14:11:16 +0200

    Small fixes from autocheks

--- lib/facter/yum_updates.rb
+++ lib/facter/yum_updates.rb
@@ -25,20 +25,20 @@ end
 Facter.add('yum_security_updates') do
   confine osfamily: 'RedHat'
   setcode do
-    yum_security_updates = Hash.new(Array.new())
+    yum_security_updates = Hash.new()
     if File.executable?('/usr/bin/yum')
       yum_get_security_result = Facter::Util::Resolution.exec('/usr/bin/yum --quiet updateinfo list security installed') #TODO: --secseverity=Moderate
       unless yum_get_security_result.nil?
         yum_get_security_result.each_line do |line|
-          _sec_code, _sec_level, package, trash = line.split(%r{\s+})
-          if trash  #some repositories make yum fill with garbage
+          _sec_code, sec_level, package, trash = line.split(%r{\s+})
+          if trash # Some repositories make yum fill with garbage
             next
           end
-          _sec_level.chomp!("/Sec.")
-          if not yum_security_updates.key?(_sec_level)
-            yum_security_updates[_sec_level] = Array.new()
+          sec_level.chomp!('/Sec.')
+          unless yum_security_updates.key?(sec_level)
+            yum_security_updates[sec_level] = []
           end
-          yum_security_updates[_sec_level].push(package)
+          yum_security_updates[sec_level].push(package)
         end
       end
     end
