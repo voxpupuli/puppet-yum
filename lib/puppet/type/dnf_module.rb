307a39fd14c6a773bed2157c5e70eb5a03fff056
Emerson Prado <emerson.prado.eng@gmail.com>
2023-06-13 16:37:07 -0300

    Add custom resource "dnf_module" parameters

--- lib/puppet/type/dnf_module.rb
+++ lib/puppet/type/dnf_module.rb
@@ -3,7 +3,22 @@
 Puppet::Type.newtype(:dnf_module) do
   @doc = <<-EOS
     @summary Manage DNF modules
+    @param title
+      Resource unique id
+    @param module
+      Module to be managed
 
     This type allows Puppet to enable/disable streams and install/remove profiles via DNF modules
 EOS
+
+  newparam(:title, namevar: true) do
+    desc 'Resource title'
+  end
+
+  newparam(:module) do
+    desc 'Module to be managed (String)'
+    validate do |value|
+      raise TypeError, 'Module name should be a string' unless value.is_a?(String)
+    end
+  end
 end
--- spec/unit/puppet/type/dnf_module_spec.rb
+++ spec/unit/puppet/type/dnf_module_spec.rb
@@ -4,7 +4,17 @@ require 'spec_helper'
 
 dnf_module = Puppet::Type.type(:dnf_module)
 RSpec.describe 'the dnf_module type' do
+  let :params do
+    %i[title module]
+  end
+
   it 'loads' do
     expect(dnf_module).not_to be_nil
+  end
+
+  it 'has expected parameters' do
+    params.each do |param|
+      expect(dnf_module.parameters).to be_include(param)
+    end
   end
 end
