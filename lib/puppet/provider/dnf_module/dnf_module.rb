cff38a772d0e2ce34c892bb8189635419123cc2f
Emerson Prado <emerson.prado.eng@gmail.com>
2023-06-13 16:50:13 -0300

    Validate module existence in custom resource "dnf_module"

--- lib/puppet/provider/dnf_module/dnf_module.rb
+++ lib/puppet/provider/dnf_module/dnf_module.rb
@@ -3,8 +3,16 @@
 Puppet::Type.type(:dnf_module).provide(:dnf_module) do
   desc 'Unique provider'
 
+  commands dnf: 'dnf'
+
+  def get_module_state(module_name)
+    dnf('-q', 'module', 'list', module_name)
+  rescue Puppet::ExecutionFailure
+    raise ArgumentError, "Module \"#{module_name}\" not found"
+  end
+
   def enabled_stream
-    nil
+    get_module_state(resource[:module])
   end
 
   def enabled_stream=(stream)
