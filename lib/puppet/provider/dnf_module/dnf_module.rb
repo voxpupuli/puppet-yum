a2539dd4f7e9278bd2c600768305690ba819cd58
Emerson Prado <emerson.prado.eng@gmail.com>
2023-06-13 17:06:28 -0300

    Start "dnf module list" parsing in custom resource "dnf_module"

--- lib/puppet/provider/dnf_module/dnf_module.rb
+++ lib/puppet/provider/dnf_module/dnf_module.rb
@@ -5,10 +5,21 @@ Puppet::Type.type(:dnf_module).provide(:dnf_module) do
 
   commands dnf: 'dnf'
 
+  def dnf_output_2_hash(dnf_output)
+    module_hash = {}
+    dnf_output.lines.each do |line|
+      line.chomp!
+      break if line.empty?
+    end
+    module_hash
+  end
+
   def get_module_state(module_name)
-    dnf('-q', 'module', 'list', module_name)
+    dnf_output = dnf('-q', 'module', 'list', module_name)
   rescue Puppet::ExecutionFailure
     raise ArgumentError, "Module \"#{module_name}\" not found"
+  else
+    @module_state = dnf_output_2_hash(dnf_output)
   end
 
   def enabled_stream
