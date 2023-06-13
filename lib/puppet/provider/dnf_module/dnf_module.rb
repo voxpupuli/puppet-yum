728ea5dabe86dd003cdc3b961a381a6f06ffef53
Emerson Prado <emerson.prado.eng@gmail.com>
2023-06-13 17:10:41 -0300

    Save module streams in custom resource "dnf_module"

--- lib/puppet/provider/dnf_module/dnf_module.rb
+++ lib/puppet/provider/dnf_module/dnf_module.rb
@@ -6,10 +6,18 @@ Puppet::Type.type(:dnf_module).provide(:dnf_module) do
   commands dnf: 'dnf'
 
   def dnf_output_2_hash(dnf_output)
-    module_hash = {}
+    module_hash = {streams: {}}
     dnf_output.lines.each do |line|
       line.chomp!
       break if line.empty?
+      if ! @stream_start.nil?
+        stream_string = line[@stream_start, @stream_length].rstrip
+        stream = stream_string.split[0]
+        module_hash[:streams][stream] = {}
+      elsif line.split[0] == 'Name'
+        @stream_start = line[/Name\s+/].length
+        @stream_length = line[/Stream\s+/].length
+      end
     end
     module_hash
   end
