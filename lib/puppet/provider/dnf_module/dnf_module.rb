daa5b25d8a7647b8de15f9bc897a8705e58d150b
Emerson Prado <emerson.prado.eng@gmail.com>
2023-07-01 16:53:00 -0300

    Add plenty of comments in custom resource "dnf_module"

--- lib/puppet/provider/dnf_module/dnf_module.rb
+++ lib/puppet/provider/dnf_module/dnf_module.rb
@@ -5,12 +5,30 @@ Puppet::Type.type(:dnf_module).provide(:dnf_module) do
 
   commands dnf: 'dnf'
 
+  # Converts plain output from 'dnf module list <Module>' to an array formatted as:
+  # {
+  #   default_stream: "<Default stream> (if there's one)",
+  #   enabled_stream: "<Enabled stream> (if there's one)",
+  #   streams: {
+  #     "<Stream>" => {
+  #       profiles: [<All available profiles for the stream>],
+  #       default_profile: "<Default profile> (if there's one)",
+  #       installed_profiles: [<All currently installed profiles>],
+  #     }
+  #   },
+  #   "<Stream>" => {...},
+  #   ...,
+  # }
   def dnf_output_2_hash(dnf_output)
     module_hash = {streams: {}}
     dnf_output.lines.each do |line|
       line.chomp!
       break if line.empty?
+      # @stream_start, @stream_length, @profiles_start and @profiles_length:
+      # chunk of dnf output line with stream or profile info
+      # Determined in elsif block below from dnf output header
       if ! @stream_start.nil?
+        # Stream string is '<Stream>', '<Stream> [d][e]', or the like
         stream_string = line[@stream_start, @stream_length].rstrip
         stream = stream_string.split[0]
         module_hash[:default_stream] = stream if stream_string.include?('[d]')
@@ -18,12 +36,15 @@ Puppet::Type.type(:dnf_module).provide(:dnf_module) do
         module_hash[:streams][stream] = {profiles: [], installed_profiles: []}
         profiles_string = line[@profiles_start, @profiles_length].rstrip
         profiles_string.split(', ').each do |profile_string|
+          # Profile string is '<Profile>', '<Profile> [d][i]', or the like
           profile = profile_string.split[0]
           module_hash[:streams][stream][:profiles] << profile
           module_hash[:streams][stream][:default_profile] = profile if profile_string.include?('[d]')
           module_hash[:streams][stream][:installed_profiles] << profile if profile_string.include?('[i]')
         end
       elsif line.split[0] == 'Name'
+        # 'dnf module list' output header is 'Name<Spaces>Stream<Spaces>Profiles<Spaces>...'
+        # Each field has same position of data that follows
         @stream_start = line[/Name\s+/].length
         @stream_length = line[/Stream\s+/].length
         @profiles_start = @stream_start + @stream_length
@@ -33,13 +54,18 @@ Puppet::Type.type(:dnf_module).provide(:dnf_module) do
     module_hash
   end
 
+  # Gets current state of stream and profiles of specified module
+  # Output formatted by function dnf_output_2_hash
   def get_module_state(module_name)
+    # This function can be called multiple times in the same resource call
     return unless @module_state.nil?
     dnf_output = dnf('-q', 'module', 'list', module_name)
   rescue Puppet::ExecutionFailure
+    # Assumes any execution error happens because module doesn't exist
     raise ArgumentError, "Module \"#{module_name}\" not found"
   else
     @module_state = dnf_output_2_hash(dnf_output)
+    # Which stream to use in functions which manage profiles
     case resource[:enabled_stream]
     when false, true
       @profiles_stream = @module_state[:default_stream]
@@ -54,6 +80,7 @@ Puppet::Type.type(:dnf_module).provide(:dnf_module) do
     dnf('-y', 'module', action, module_spec)
   end
 
+  # Checks if all specified profiles exist in module's stream
   def validate_profiles(module_name, specified, existing)
     invalid = specified - existing
     raise ArgumentError, "Profile(s) #{invalid.map{ |profile| "\"#{profile}\""}.join(', ')} " +
@@ -62,8 +89,10 @@ Puppet::Type.type(:dnf_module).provide(:dnf_module) do
 
   def enabled_stream
     get_module_state(resource[:module])
+    # Specified stream = nil requires a previously enabled stream
     raise ArgumentError, "No enabled stream to keep in module \"#{resource[:module]}\"" if
       resource[:enabled_stream].nil? and ! @module_state.key?(:enabled_stream)
+    # Specified stream = true requires an existing default stream
     raise ArgumentError, "No default stream to enable in module \"#{resource[:module]}\"" if
       resource[:enabled_stream] == true and ! @module_state.key?(:default_stream)
     return nil if resource[:enabled_stream].nil?
@@ -86,6 +115,7 @@ Puppet::Type.type(:dnf_module).provide(:dnf_module) do
 
   def installed_profiles
     get_module_state(resource[:module])
+    # Profiles exist inside stream
     if @profiles_stream.nil?
       return [] if resource[:installed_profiles].empty?
       raise ArgumentError, "No enabled or default stream in module \"#{resource[:module]}\""
@@ -93,11 +123,14 @@ Puppet::Type.type(:dnf_module).provide(:dnf_module) do
     stream_contents = @module_state[:streams][@profiles_stream]
     installed = stream_contents[:installed_profiles]
     if resource[:installed_profiles] == [true]
+      # Specified profile = true requires an existing default profile
       raise ArgumentError, "No default profile to install in module:stream \"#{resource[:module]}:#{@profiles_stream}\"" unless
         stream_contents.key?(:default_profile)
+      # Act if default profile isn't installed
       installed.include?(stream_contents[:default_profile]) ? [true] : []
     else
       validate_profiles(resource[:module], resource[:installed_profiles], stream_contents[:profiles])
+      # Only installed profiles included in specified ones are relevant here
       installed & resource[:installed_profiles]
     end
   end
