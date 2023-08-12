2486a5fcb0985c365a3cab180d1b50e47608d648
Emerson Prado <emerson.prado.eng@gmail.com>
2023-08-12 18:46:26 -0300

    Get state from dnf output in dnf_module_stream custom resource provider

--- lib/puppet/provider/dnf_module_stream/dnf_module_stream.rb
+++ lib/puppet/provider/dnf_module_stream/dnf_module_stream.rb
@@ -7,19 +7,35 @@ Puppet::Type.type(:dnf_module_stream).provide(:dnf_module_stream) do
 
   commands dnf: 'dnf'
 
+  # Converts plain output from 'dnf module list <Module>' to an array formatted as:
+  # {
+  #   default_stream: "<Default stream> (if there's one)",
+  #   enabled_stream: "<Enabled stream> (if there's one)",
+  #   available_streams: ["<Stream>", "<Stream>", ...,]
+  # }
   def dnf_output_2_hash(dnf_output)
+    module_hash = { available_streams: [] }
     dnf_output.lines.each do |line|
       line.chomp!
       break if line.empty?
 
       # @stream_start and @stream_length: chunk of dnf output line with stream info
-      if line.split[0] == 'Name'
+      # Determined in elsif block below from dnf output header
+      if !@stream_start.nil?
+        # Stream string is '<Stream>', '<Stream> [d][e]', or the like
+        stream_string = line[@stream_start, @stream_length].rstrip
+        stream = stream_string.split[0]
+        module_hash[:default_stream] = stream if stream_string.include?('[d]')
+        module_hash[:enabled_stream] = stream if stream_string.include?('[e]')
+        module_hash[:available_streams] << stream
+      elsif line.split[0] == 'Name'
         # 'dnf module list' output header is 'Name<Spaces>Stream<Spaces>Profiles<Spaces>...'
         # Each field has same position of data that follows
         @stream_start = line[%r{Name\s+}].length
         @stream_length = line[%r{Stream\s+}].length
       end
     end
+    module_hash
   end
 
   # Gets module default, enabled and available streams
@@ -28,10 +44,12 @@ Puppet::Type.type(:dnf_module_stream).provide(:dnf_module_stream) do
     # This function can be called multiple times in the same resource call
     return unless @streams_current_state.nil?
 
-    @streams_current_state = dnf('-q', 'module', 'list', module_name)
+    dnf_output = dnf('-q', 'module', 'list', module_name)
   rescue Puppet::ExecutionFailure
     # Assumes any execution error happens because module doesn't exist
     raise ArgumentError, "Module \"#{module_name}\" not found"
+  else
+    @streams_current_state = dnf_output_2_hash(dnf_output)
   end
 
   def stream
