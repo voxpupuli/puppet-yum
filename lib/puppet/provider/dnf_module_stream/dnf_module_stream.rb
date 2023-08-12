68d1b0489e1200e9804f50791227972955ca6ded
Emerson Prado <emerson.prado.eng@gmail.com>
2023-08-12 18:43:14 -0300

    Query module stream state in dnf_module_stream custom resource provider

--- lib/puppet/provider/dnf_module_stream/dnf_module_stream.rb
+++ lib/puppet/provider/dnf_module_stream/dnf_module_stream.rb
@@ -7,6 +7,18 @@ Puppet::Type.type(:dnf_module_stream).provide(:dnf_module_stream) do
 
   commands dnf: 'dnf'
 
+  # Gets module default, enabled and available streams
+  # Output formatted by function dnf_output_2_hash
+  def streams_state(module_name)
+    # This function can be called multiple times in the same resource call
+    return unless @streams_current_state.nil?
+
+    @streams_current_state = dnf('-q', 'module', 'list', module_name)
+  rescue Puppet::ExecutionFailure
+    # Assumes any execution error happens because module doesn't exist
+    raise ArgumentError, "Module \"#{module_name}\" not found"
+  end
+
   def stream
     nil
   end
