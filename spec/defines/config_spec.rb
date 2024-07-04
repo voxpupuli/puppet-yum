e1070c8858cca5d0632458dac90bd235bc3a2c6a
Brideau, Patrick <patrick.brideau@teluq.ca>
2024-07-04 13:07:37 -0400

    test: run config and versionlock on every supported OS

--- spec/defines/config_spec.rb
+++ spec/defines/config_spec.rb
@@ -3,98 +3,106 @@
 require 'spec_helper'
 
 describe 'yum::config' do
-  context 'with no parameters' do
-    let(:title) { 'assumeyes' }
+  on_supported_os.each do |os, os_facts|
+    context "on #{os}" do
+      let(:facts) { os_facts }
 
-    it { is_expected.to compile.and_raise_error(%r{expects a value for parameter 'ensure'}) }
-  end
-
-  %w[dnf yum].each do |pkgmgr|
-    context "when package_provider fact is #{pkgmgr}" do
-      let(:facts) { { package_provider: pkgmgr } }
-
-      context 'when ensure is a Boolean' do
+      context 'with no parameters' do
         let(:title) { 'assumeyes' }
-        let(:params) { { ensure: true } }
 
-        it { is_expected.to compile.with_all_deps }
-
-        it 'contains an Augeas resource with the correct changes' do
-          case pkgmgr
-          when 'yum'
-            is_expected.to contain_augeas("yum.conf_#{title}").with(
-              incl: '/etc/yum.conf',
-              context: '/files/etc/yum.conf/main/',
-              changes: "set assumeyes '1'"
-            )
-          else
-            is_expected.to contain_augeas("dnf.conf_#{title}").with(
-              incl: '/etc/dnf/dnf.conf',
-              context: '/files/etc/dnf/dnf.conf/main/',
-              changes: "set assumeyes '1'"
-            )
-          end
-        end
+        it { is_expected.to compile.and_raise_error(%r{expects a value for parameter 'ensure'}) }
       end
 
-      context 'ensure is an Integer' do
-        let(:title) { 'assumeyes' }
-        let(:params) { { ensure: 0 } }
-
-        it { is_expected.to compile.with_all_deps }
-
-        it 'contains an Augeas resource with the correct changes' do
-          case pkgmgr
-          when 'yum'
-            is_expected.to contain_augeas("yum.conf_#{title}").with(
-              changes: "set assumeyes '0'"
-            )
-          else
-            is_expected.to contain_augeas("dnf.conf_#{title}").with(
-              changes: "set assumeyes '0'"
-            )
+      %w[dnf yum].each do |pkgmgr|
+        context "when package_provider fact is #{pkgmgr}" do
+          let(:facts) do
+            super().merge({ package_provider: pkgmgr })
           end
-        end
-      end
 
-      context 'ensure is a comma separated String' do
-        let(:title) { 'assumeyes' }
-        let(:params) { { ensure: '1, 2' } }
+          context 'when ensure is a Boolean' do
+            let(:title) { 'assumeyes' }
+            let(:params) { { ensure: true } }
 
-        it { is_expected.to compile.with_all_deps }
+            it { is_expected.to compile.with_all_deps }
 
-        it 'contains an Augeas resource with the correct changes' do
-          case pkgmgr
-          when 'yum'
-            is_expected.to contain_augeas("yum.conf_#{title}").with(
-              changes: "set assumeyes '1, 2'"
-            )
-          else
-            is_expected.to contain_augeas("dnf.conf_#{title}").with(
-              changes: "set assumeyes '1, 2'"
-            )
+            it 'contains an Augeas resource with the correct changes' do
+              case pkgmgr
+              when 'yum'
+                is_expected.to contain_augeas("yum.conf_#{title}").with(
+                  incl: '/etc/yum.conf',
+                  context: '/files/etc/yum.conf/main/',
+                  changes: "set assumeyes '1'"
+                )
+              else
+                is_expected.to contain_augeas("dnf.conf_#{title}").with(
+                  incl: '/etc/dnf/dnf.conf',
+                  context: '/files/etc/dnf/dnf.conf/main/',
+                  changes: "set assumeyes '1'"
+                )
+              end
+            end
           end
-        end
-      end
 
-      context 'when ensure is a Sensitive[String]' do
-        let(:title) { 'assumeyes' }
-        let(:params) { { ensure: sensitive('secret') } }
+          context 'ensure is an Integer' do
+            let(:title) { 'assumeyes' }
+            let(:params) { { ensure: 0 } }
 
-        it { is_expected.to compile.with_all_deps }
+            it { is_expected.to compile.with_all_deps }
 
-        it 'contains an Augeas resource with the correct changes' do
-          case pkgmgr
-          when 'yum'
-            is_expected.to contain_augeas("yum.conf_#{title}").with(
-              changes: "set assumeyes 'secret'",
-              show_diff: false
-            )
-          else
-            is_expected.to contain_augeas("dnf.conf_#{title}").with(
-              changes: "set assumeyes 'secret'",
-              show_diff: false
-            )
+            it 'contains an Augeas resource with the correct changes' do
+              case pkgmgr
+              when 'yum'
+                is_expected.to contain_augeas("yum.conf_#{title}").with(
+                  changes: "set assumeyes '0'"
+                )
+              else
+                is_expected.to contain_augeas("dnf.conf_#{title}").with(
+                  changes: "set assumeyes '0'"
+                )
+              end
+            end
+          end
+
+          context 'ensure is a comma separated String' do
+            let(:title) { 'assumeyes' }
+            let(:params) { { ensure: '1, 2' } }
+
+            it { is_expected.to compile.with_all_deps }
+
+            it 'contains an Augeas resource with the correct changes' do
+              case pkgmgr
+              when 'yum'
+                is_expected.to contain_augeas("yum.conf_#{title}").with(
+                  changes: "set assumeyes '1, 2'"
+                )
+              else
+                is_expected.to contain_augeas("dnf.conf_#{title}").with(
+                  changes: "set assumeyes '1, 2'"
+                )
+              end
+            end
+          end
+
+          context 'when ensure is a Sensitive[String]' do
+            let(:title) { 'assumeyes' }
+            let(:params) { { ensure: sensitive('secret') } }
+
+            it { is_expected.to compile.with_all_deps }
+
+            it 'contains an Augeas resource with the correct changes' do
+              case pkgmgr
+              when 'yum'
+                is_expected.to contain_augeas("yum.conf_#{title}").with(
+                  changes: "set assumeyes 'secret'",
+                  show_diff: false
+                )
+              else
+                is_expected.to contain_augeas("dnf.conf_#{title}").with(
+                  changes: "set assumeyes 'secret'",
+                  show_diff: false
+                )
+              end
+            end
           end
         end
       end
